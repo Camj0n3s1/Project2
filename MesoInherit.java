aa01d1006292eb3fa7eafb490c1d396e17ac71f7
Camron Jones <Camron.d.jones-1@ou.edu>
2019-10-10 16:35:33 -0500

    Created calAverage and letterAverage for the MesoInherit class

--- MesoInherit.java
+++ MesoInherit.java
@@ -1,4 +1,15 @@
 
-public class MesoInherit {
+public class MesoInherit extends MesoAbstract {
+	
+	//Method that returns an array the floor, ceiling, and average
+	public int[] calAverage() {
+		
+	}
+	
+	//Method that returns the letter average for the output
+	@Override
+	public char letterAverage() {
+		
+	}
 	
 }
