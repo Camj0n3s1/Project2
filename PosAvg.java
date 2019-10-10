267c05781efcc062cb1b268485194d4e5e74cbc3
Camron Jones <Camron.d.jones-1@ou.edu>
2019-10-10 16:24:37 -0500

    Finished toString method of PosAvg

--- PosAvg.java
+++ PosAvg.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class PosAvg {
 	private String stID;
 	ArrayList<String> file = new ArrayList<String>();
+
 	// Constructor
 	public PosAvg() {
 
@@ -19,16 +20,16 @@ public class PosAvg {
 	// Reads the Mesonet.txt
 	public void fileReader(String filename) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader("Mesonet.txt"));
-		
-		//Skip over redundant 3 lines
+
+		// Skip over redundant 3 lines
 		br.readLine();
 		br.readLine();
 		br.readLine();
-		
-		//Grab first line
+
+		// Grab first line
 		String line = br.readLine();
-		while(line != null) {
-			stID = line.substring(1,4);
+		while (line != null) {
+			stID = line.substring(1, 4);
 			file.add(stID);
 			br.readLine();
 		}
@@ -39,22 +40,28 @@ public class PosAvg {
 	public int indexOfStation() {
 		String target = stID;
 		int index = 0;
-		for(int i = 0; i < file.size(); ++i) {
+		for (int i = 0; i < file.size(); ++i) {
 			String stationMatch = file.get(i);
-			if(stationMatch.equalsIgnoreCase(target)) {
+			if (stationMatch.equalsIgnoreCase(target)) {
 				index = file.indexOf(stationMatch);
 			}
 		}
 		return index;
 	}
-	
-	// 
-	public int averageOf() {
-		
-	}
+
+	// Gives the 2 line of output needed
 	@Override
 	public String toString() {
-		
+
+		// Gives the station 2 below index and 2 above index
+		String oneBelowIndex = file.get(indexOfStation() - 1);
+		String twoBelowIndex = file.get(indexOfStation() - 2);
+		String oneAboveIndex = file.get(indexOfStation() + 1);
+		String twoAboveIndex = file.get(indexOfStation() + 2);
+
+		// Returns 2 line of output
+		return String.format("This index is the average of " + oneBelowIndex + " and " + oneAboveIndex + ", "
+				+ twoBelowIndex + " and " + twoAboveIndex + ", and so on");
 	}
-	
+
 }
